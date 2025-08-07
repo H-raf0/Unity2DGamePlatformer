7a1bd990f72fb5fa86511176a5f63f94d5f2c983
Achraf <achrafelallali123@gmail.com>
2025-08-07 18:54:47 +0200

    fixing the color changing's behavior of gameover's button

--- Assets/Scripts/GameoverScript.cs
+++ Assets/Scripts/GameoverScript.cs
@@ -10,11 +10,5 @@ public class GameoverScript : MonoBehaviour
     public void GameOver()
     {
         gameObject.SetActive(true);  // Activate the game over UI
-
-        // Clear any previous selections
-        EventSystem.current.SetSelectedGameObject(null);
-
-        // Set a new default selection, pressing "enter" will trigger the selection
-        EventSystem.current.SetSelectedGameObject(restartButton.gameObject);
     }
 }
